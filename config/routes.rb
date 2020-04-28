4c018ed79296b6576ae5254027bff06c9bfc4239
René Sprotte <renspr@gmail.com>
2020-04-28 16:24:51 +0200

    Change path for partial api.

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   get  "/auslass", to: "access#exit_index", as: "exit_index"
   post "/auslass", to: "access#exit", as: "exit"
 
-  get "/stats", to: "access#stats", as: "stats"
-  get "/log", to: "access#log", as: "log"
+  get "/api/stats", to: "access#stats", as: "stats"
+  get "/api/log", to: "access#log", as: "log"
 
   namespace :admin, path: "/admin" do
     get "/reset", to: "reset#index", as: "reset_index"
