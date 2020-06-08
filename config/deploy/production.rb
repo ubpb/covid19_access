430aae5426fb3ddff800465e7566141da828def1
René Sprotte <renspr@gmail.com>
2020-06-08 13:27:24 +0200

    Add ubatch18 for running cronjobs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
 server "ubpegasus18.upb.de", user: "ubpb", roles: %w{app web db}
 server "ubperseus18.upb.de", user: "ubpb", roles: %w{app web}
+server "ubatch18.upb.de",    user: "ubpb", roles: %w{app db}
 set :deploy_to, "/ubpb/covid19access"
 set :branch, "production"
