19a840970323435be8dc05244baef27bc9e17135
René Sprotte <renspr@gmail.com>
2020-07-11 20:03:31 +0200

    Use update_attribute to skip validation.

--- app/controllers/admin/reset_controller.rb
+++ app/controllers/admin/reset_controller.rb
@@ -2,7 +2,7 @@ class Admin::ResetController < Admin::ApplicationController
 
   def create
     Registration.where(exited_at: nil).each do |p|
-      p.update(exited_at: p.entered_at.end_of_day)
+      p.update_attribute(:exited_at, p.entered_at.end_of_day)
     end
 
     flash[:success] = "Reset erfolgreich durchgeführt."
