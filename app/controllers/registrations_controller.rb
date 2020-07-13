7eea76e660ad69cfc3af49aa0d4e8442558074d4
René Sprotte <renspr@gmail.com>
2020-07-13 12:17:27 +0200

    Tweeks

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,8 +5,22 @@ class RegistrationsController < ApplicationController
       verify_ilsid(ilsid, entries_path) or return
       verify_registration_for(ilsid) or return
 
+      # Load data from Aleph
       bib_data = AlephClient.new.get_bib_data_for(ilsid)
       verify_bib_data(ilsid, bib_data) or return
+
+      # Try to find a previous registration that has more data to speed
+      # up the registration process.
+      unless params[:reload_aleph_data]
+        previous_registration = Registration.order("entered_at desc").find_by(ilsid: ilsid)
+        if previous_registration.present?
+          bib_data[:name]   = previous_registration.name.presence   || bib_data[:name]
+          bib_data[:street] = previous_registration.street.presence || bib_data[:street]
+          bib_data[:city]   = previous_registration.city.presence   || bib_data[:city]
+          bib_data[:phone]  = previous_registration.phone.presence  || bib_data[:phone]
+          bib_data[:email]  = previous_registration.email.presence  || bib_data[:email]
+        end
+      end
 
       @registration        = Registration.new(ilsid: ilsid)
       @registration.name   = bib_data[:name]
