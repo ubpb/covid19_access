40b3238ade778082e903c7ff2304a49f459e97a3
René Sprotte <renspr@gmail.com>
2020-09-07 15:35:17 +0200

    Add homepage.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,11 @@
 class PagesController < ApplicationController
+
+  def homepage
+    now = Time.zone.now
+    @number_of_people_entered = Registration.number_of_people_entered
+    @max_number_of_people = Rails.configuration.application.max_people || 40
+    @number_of_people_entered_last_hour = Registration.where(entered_at: (now - 1.hour)..now).count
+    @number_of_people_exited_last_hour = Registration.where(exited_at: (now - 1.hour)..now).count
+  end
+
 end
