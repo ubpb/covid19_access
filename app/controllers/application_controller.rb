815c8045e3a5d1555f84aedd522869f958054a55
René Sprotte <renspr@gmail.com>
2020-09-19 22:11:55 +0200

    DRY stats.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def set_global_stats(now = Time.zone.now)
+    @number_of_people_entered = Registration.number_of_people_entered
+    @max_number_of_people = Rails.configuration.application.max_people || 40
+    @number_of_people_entered_last_hour = Registration.where(entered_at: (now - 1.hour)..now).count
+    @number_of_people_exited_last_hour = Registration.where(exited_at: (now - 1.hour)..now).count
+    @opening_time, @closing_time = Reservation.get_opening_and_closing_times(Time.zone.today)
+  end
+
 private
 
   def authenticate!
--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,6 +3,7 @@ class StatisticsController < ApplicationController
   def index
     @resource_statistics = []
     now = Time.zone.now
+    set_global_stats(now)
 
     ResourceGroup.includes(:resources).order(:title).each do |rg|
       resources = rg.resources.joins(:resource_location).includes(:allocation, :reservations).order("resource_locations.title, resources.title").to_a
@@ -22,11 +23,6 @@ class StatisticsController < ApplicationController
         number_of_released_allocations_last_hour: number_of_released_allocations_last_hour
       }
     end
-
-    @number_of_people_entered = Registration.number_of_people_entered
-    @max_number_of_people = Rails.configuration.application.max_people || 40
-    @number_of_people_entered_last_hour = Registration.where(entered_at: (now - 1.hour)..now).count
-    @number_of_people_exited_last_hour = Registration.where(exited_at: (now - 1.hour)..now).count
   end
 
 end
